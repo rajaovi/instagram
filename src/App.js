ed076e4d3f0df255945fe73c924da9c1eba06de2
Raja <orajacse@gmail.com>
2023-07-29 15:50:17 +0530

    Link Login to Dashboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import "./common/style/common.scss";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,48 +1,87 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./index.scss";
 
 const Login = () => {
   const [userName, setUserName] = useState("");
   const [userPassword, setuserPassword] = useState("");
+  const [useList, setUsersList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUsersList(data);
+      });
+  };
+
   return (
     <div className="loginPage">
-      <div className="loginArea">
+      <div className="loginArea container">
         <div className="loginIntro">
-          <img src="../../logo.jpg" alt="Intro" />
+          <img src={require("../../images/intro.jpg")} alt="Intro" />
         </div>
         <div className="loginForm">
-          <div className="logoLogin">
-            <img src="../../logo.jpg" alt="Login Logo" />
-          </div>
-          <form>
+          <div>
+            <div className="logoLogin">
+              <img
+                src={require("../../images/logoIntro.jpg")}
+                alt="Login Logo"
+              />
+            </div>
+            <form action="">
+              <div>
+                <p>
+                  <input
+                    type="text"
+                    value={userName}
+                    onChange={(e) => {
+                      setUserName(e.target.value);
+                    }}
+                    placeholder="Phone number, username, or email"
+                  />
+                </p>
+                <p>
+                  <input
+                    type="password"
+                    value={userPassword}
+                    onChange={(e) => {
+                      setuserPassword(e.target.value);
+                    }}
+                    placeholder="Password"
+                  />
+                </p>
+                <div>
+                  <Link to="/dashboard">
+                    <button onClick={handleSubmit}>Log in</button>
+                  </Link>
+                </div>
+              </div>
+            </form>
             <div>
+              <div className="loginElse">
+                <p>OR</p>
+              </div>
+              <div>
+                <p>Log in with facebook</p>
+              </div>
               <p>
-                <input
-                  type="text"
-                  value={userName}
-                  onChange={(e) => {
-                    setUserName(e.target.value);
-                  }}
-                  placeholder="Phone number, username, or email"
-                />
-              </p>
-              <p>
-                <input
-                  type="password"
-                  value={userPassword}
-                  onChange={(e) => {
-                    setuserPassword(e.target.value);
-                  }}
-                  placeholder="Password"
-                />
+                Sorry, your password was incorrect. Please double-check your
+                password.
               </p>
               <div>
-                <Link to="/dashboard">
-                  <button>Log in</button>
-                </Link>
+                <p>forgot password</p>
               </div>
             </div>
-          </form>
+          </div>
+          <div className="signUp">
+            <p>
+              Don't have an account? <Link to="/signup">Sign up</Link>
+            </p>
+          </div>
         </div>
       </div>
     </div>
